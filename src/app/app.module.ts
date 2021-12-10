b0c81c13c1e6b5d57af801891c3e19e9f1224246
LinaMalinaMeow <74879251+LinaMalinaMeow@users.noreply.github.com>
2021-12-10 15:26:41 +0300

    Add buttons and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, Validators} from "@angular/forms";
+import {registerLocaleData} from "@angular/common";
+import localeRu from '@angular/common/locales/ru';
 
 import { AppComponent } from './app.component';
+import {MessageComponent} from './message/message.component';
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from "primeng/ripple";
+import {MessageModule} from "primeng/message";
+import {InputTextModule} from "primeng/inputtext";
+import {TableModule} from 'primeng/table';
 
+registerLocaleData(localeRu, 'ru')
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MessageComponent, ],
+  imports: [BrowserModule, FormsModule, ButtonModule, RippleModule, MessageModule, InputTextModule, TableModule,],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
