55c7dad5d880d75f085384473f50c66a7057b825
LinaMalinaMeow <74879251+LinaMalinaMeow@users.noreply.github.com>
2021-12-10 20:35:36 +0300

    Add buttons and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, Validators} from "@angular/forms";
 import {registerLocaleData} from "@angular/common";
@@ -11,17 +12,27 @@ import {RippleModule} from "primeng/ripple";
 import {MessageModule} from "primeng/message";
 import {InputTextModule} from "primeng/inputtext";
 import {TableModule} from 'primeng/table';
+import {LoginPageComponent} from "./login-page/login-page.component";
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'main', component: MessageComponent},
+  { path: '',   redirectTo: 'login', pathMatch: 'full' }
+];
 registerLocaleData(localeRu, 'ru')
 @NgModule({
-  declarations: [AppComponent, MessageComponent, ],
   imports: [BrowserModule,
     FormsModule,
     ButtonModule,
     RippleModule,
     MessageModule,
     InputTextModule,
+    RouterModule.forRoot(routes),
     TableModule,],
+  declarations: [AppComponent, MessageComponent, LoginPageComponent ],
+
+
   providers: [],
   bootstrap: [AppComponent],
 })
